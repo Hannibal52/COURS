be05e89c9874285d7ab68d81776c8d66e815020b
Hannibal52 <86859477+Hannibal52@users.noreply.github.com>
2023-04-13 14:35:27 +0200

    Delete cisco.php

--- Réseaux/CISCO/cisco.php
+++
@@ -1,5 +0,0 @@
-<?php include('1/RS_instructorPPT_Chapter1.html'); ?>
-<?php include('2/RS_instructorPPT_Chapter2.html'); ?>
-<?php include('3/RS_instructorPPT_Chapter3.html'); ?>
-<?php include('4/RS_instructorPPT_Chapter4.html'); ?>
-<?php include('5/RS_instructorPPT_Chapter5.html'); ?>
